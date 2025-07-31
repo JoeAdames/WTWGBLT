e1c529d379a776cc19e2620a9d60a533de84a989
JDA <jadames14@gmail.com>
2025-07-31 12:02:49 -0400

    import switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "./context/ThemeContext.jsx";
+import { ForeCastProvider } from "./context/ForeCastContext.jsx";
 import { ForeCastTypeProvider } from "./context/ForeCastTypeContext.jsx";
 import { LocationProvider } from "./context/LocationsContext.jsx";
 import { BadgeProvider } from "./context/BadgeContext.jsx";
 import Dashboard from "./pages/Dashboard";
-import { ForeCastProvider } from "./context/ForecastContext.jsx";
 
 function App() {
   return (
