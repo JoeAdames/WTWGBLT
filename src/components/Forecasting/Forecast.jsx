a0848e6da3c2ce514128158d2e5a99e6f69b7061
JDA <jadames14@gmail.com>
2025-07-31 12:11:42 -0400

    what a dunce huh

--- src/components/Forecasting/Forecast.jsx
+++ src/components/Forecasting/Forecast.jsx
@@ -1,23 +1,29 @@
-import React, {useState, useEffect} from 'react'
-import LongForcast from './LongForcast';
-import ShortForecast from './ShortForecast';
-import { useForeCastType } from '../../context/ForeCastTypeContext';
-import { useForeCast } from '../../context/ForecastContext';
-import Toggle from '../Toggle';
-
+import React, { useState, useEffect } from "react";
+import LongForcast from "./LongForcast";
+import ShortForecast from "./ShortForecast";
+import { useForeCastType } from "../../context/ForeCastTypeContext";
+import { useForeCast } from "../../context/ForeCastContext";
+import Toggle from "../Toggle";
 
 export default function Forecast() {
-  const {foreCastType, toggleForeCastType} = useForeCastType();
-  const {data, loading, error } = useForeCast();
+  const { foreCastType, toggleForeCastType } = useForeCastType();
+  const { data, loading, error } = useForeCast();
 
-
-  if(loading) return <div>Loading...</div>;
-  if(error) return <div>Error: {error.message}</div>
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
   return (
-    <div className='flex flex-col sm:items-center'>
-      <Toggle handleClick={toggleForeCastType}  current={foreCastType} style={'flex justify-end pl-3'}/>
-        {foreCastType == "Today" ? <ShortForecast current={data.periods[0]} later={data.periods[1]}/> : <LongForcast data={data.periods} /> }
+    <div className="flex flex-col sm:items-center">
+      <Toggle
+        handleClick={toggleForeCastType}
+        current={foreCastType}
+        style={"flex justify-end pl-3"}
+      />
+      {foreCastType == "Today" ? (
+        <ShortForecast current={data.periods[0]} later={data.periods[1]} />
+      ) : (
+        <LongForcast data={data.periods} />
+      )}
     </div>
-  )
+  );
 }
